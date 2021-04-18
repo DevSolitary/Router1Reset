96be2f2c0921c0b51a82a44f6ad9bed554d500f2
DevSolitary <devsolitary@gmail.com>
2021-04-18 10:45:30 +0300

    fix connection

--- index.php
+++ index.php
@@ -1,56 +1,65 @@
 <?php
-	$ip = '192.168.88.221';
-	$proxy = '87.255.10.12:5999:formade:formade2';
-	$proxyauth = 'formade:formade2';
+	$ip = '192.168.88.221'; //IP
+	$proxy = '87.255.10.12:5999'; //Адрес и порт прокси
+	$proxyauth = 'formade:formade2'; //Автоизация прокси
+	$tclkey = 'KSDHSDFOGQ5WERYTUIQWERTYUISDFG1HJZXCVCXBN2GDSMNDHKVKFsVBNf'; //Ключ
+	$login = "admin"; //Логин
+	$password = "admin"; //Пароль
 
 
 	$headers = array(
-		'_TclRequestVerificationKey: KSDHSDFOGQ5WERYTUIQWERTYUISDFG1HJZXCVCXBN2GDSMNDHKVKFsVBNf',
-		'_TclRequestVerificationToken: df76bgjo32;1_[YZ',
+		'_TclRequestVerificationKey: '.$tclkey,
 		'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.72 Safari/537.36',
-		'Referer: http://192.168.88.221/index.html',
+		'Referer: http://'.$ip.'/index.html',
 		'Content-Type: application/json;charset=UTF-8'
-	);
+	); 
 
-	//$data = array("id" => "12","jsonrpc" => "2.0","method"=>"GetCurrentLanguage","params" => array ());
-	$data = array("id" => "12","jsonrpc" => "2.0","method"=>"Login","params" => array ("UserName"=>"dc13ibej?7","Password"=>"dc13ibej?7"));
+	//Авторизация
+
+	$data = array("id" => "12","jsonrpc" => "2.0","method"=>"Login","params" => array ("UserName"=>encrypter($login),"Password"=>encrypter($password)));
 	$postdata = json_encode($data);
 
 
-	$ch = curl_init('http://192.168.88.221/jrd/webapi');
-	curl_setopt($ch, CURLOPT_TIMEOUT, 4000);
-	curl_setopt($ch, CURLOPT_PROXY, $proxy); 
-	curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
-	curl_setopt($ch, CURLOPT_POST, true);
-	curl_setopt($ch, CURLOPT_POSTFIELDS, $postdata); 
+	$ch = curl_init('http://'.$ip.'/jrd/webapi'); //Адрес запроса к API
+	curl_setopt($ch, CURLOPT_TIMEOUT, 4000); // Допустимое время выполнения запроса
+	curl_setopt($ch, CURLOPT_PROXY, $proxy); // Адрес и порт прокси
+	curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth); //Логин и пароль прокси
+	curl_setopt($ch, CURLOPT_POST, true); // Тип запроса = POST
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $postdata); // Отдача параметров в JSON
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-	curl_setopt($ch, CURLOPT_HEADER, false);
-	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // Отключаем проверку SSL
+	curl_setopt($ch, CURLOPT_HEADER, false); // Отключаем выдачу заголовков
+	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers); // Заголовки запроса
 	$html = curl_exec($ch);
 	$token = json_decode($html);
-	$token = $token->result->token;
+
+
+	$token = $token->result->token; //Получаем в ответе токен, из которого сгенерируем _TclRequestVerificationToken
+
+
 	curl_close($ch);
+
 
 	$tcl_key = encrypter(strval($token));
 
+
 	sleep(2);
 
+	// Отключаем сеть
+
 	$headers = array(
-		'_TclRequestVerificationKey: KSDHSDFOGQ5WERYTUIQWERTYUISDFG1HJZXCVCXBN2GDSMNDHKVKFsVBNf',
+		'_TclRequestVerificationKey: '.$tclkey,
 		'_TclRequestVerificationToken: '.$tcl_key,
 		'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.72 Safari/537.36',
-		'Referer: http://192.168.88.221/index.html',
+		'Referer: http://'.$ip.'/index.html',
 		'Content-Type: application/json;charset=UTF-8'
 	);
 
-
-	//$data = array("id" => "12","jsonrpc" => "2.0","method"=>"GetCurrentLanguage","params" => array ());
 	$data = array("id" => "12","jsonrpc" => "2.0","method"=>"DisConnect","params" => array ());
 	$postdata = json_encode($data);
 
 
-	$ch = curl_init('http://192.168.88.221/jrd/webapi');
+	$ch = curl_init('http://'.$ip.'/jrd/webapi');
 	curl_setopt($ch, CURLOPT_TIMEOUT, 4000);
 	curl_setopt($ch, CURLOPT_PROXY, $proxy); 
 	curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
@@ -65,30 +74,13 @@
 
 	sleep(2);
 
-	$data = array("id" => "12","jsonrpc" => "2.0","method"=>"SetNetworkSettings","params" => array ("NetworkMode"=>3,"NetselectionMode"=>0,"NetworkBand"=>255,"DomesticRoam"=>0,"DomesticRoamGuard"=>0));
-	$postdata = json_encode($data);
-
-
-	$ch = curl_init('http://192.168.88.221/jrd/webapi');
-	curl_setopt($ch, CURLOPT_TIMEOUT, 4000);
-	curl_setopt($ch, CURLOPT_PROXY, $proxy); 
-	curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
-	curl_setopt($ch, CURLOPT_POST, true);
-	curl_setopt($ch, CURLOPT_POSTFIELDS, $postdata); 
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-	curl_setopt($ch, CURLOPT_HEADER, false);
-	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-	$html = curl_exec($ch);
-	curl_close($ch);
-
-	sleep(2);
+	// Ставим режим авто
 
 	$data = array("id" => "12","jsonrpc" => "2.0","method"=>"SetNetworkSettings","params" => array ("NetworkMode"=>0,"NetselectionMode"=>0,"NetworkBand"=>255,"DomesticRoam"=>0,"DomesticRoamGuard"=>0));
 	$postdata = json_encode($data);
 
 
-	$ch = curl_init('http://192.168.88.221/jrd/webapi');
+	$ch = curl_init('http://'.$ip.'/jrd/webapi');
 	curl_setopt($ch, CURLOPT_TIMEOUT, 4000);
 	curl_setopt($ch, CURLOPT_PROXY, $proxy); 
 	curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
@@ -102,12 +94,14 @@
 	curl_close($ch);
 
 	sleep(2);
+
+	// Ставим режим LTE
 
 	$data = array("id" => "12","jsonrpc" => "2.0","method"=>"SetNetworkSettings","params" => array ("NetworkMode"=>3,"NetselectionMode"=>0,"NetworkBand"=>255,"DomesticRoam"=>0,"DomesticRoamGuard"=>0));
 	$postdata = json_encode($data);
 
 
-	$ch = curl_init('http://192.168.88.221/jrd/webapi');
+	$ch = curl_init('http://'.$ip.'/jrd/webapi');
 	curl_setopt($ch, CURLOPT_TIMEOUT, 4000);
 	curl_setopt($ch, CURLOPT_PROXY, $proxy); 
 	curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
@@ -122,30 +116,43 @@
 
 	sleep(2);
 
-	$data = array("id" => "12","jsonrpc" => "2.0","method"=>"Connect","params" => array ());
+	// Подключаем соединение. Ждем, пока на наш запрос придет успешный результат
+
+	$is_connected = false;
+
+	while(!$is_connected)
+	{
+		$data = array("id" => "12","jsonrpc" => "2.0","method"=>"Connect","params" => array ());
+		$postdata = json_encode($data);
+
+
+		$ch = curl_init('http://'.$ip.'/jrd/webapi');
+		curl_setopt($ch, CURLOPT_TIMEOUT, 4000);
+		curl_setopt($ch, CURLOPT_PROXY, $proxy); 
+		curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
+		curl_setopt($ch, CURLOPT_POST, true);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $postdata); 
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		curl_setopt($ch, CURLOPT_HEADER, false);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+		$html = curl_exec($ch);
+		echo $html."<br>";
+		$res = json_decode($html);
+		$is_connected = isset($res->result);
+
+		curl_close($ch);
+
+		sleep(2);
+	}
+
+	//Выходим, что бы следующий скрипт начал работать с авторизации без ошибок
+
+	$data = array("id" => "12","jsonrpc" => "2.0","method"=>"Logout","params" => array ()); //Выход
 	$postdata = json_encode($data);
 
 
-	$ch = curl_init('http://192.168.88.221/jrd/webapi');
-	curl_setopt($ch, CURLOPT_TIMEOUT, 4000);
-	curl_setopt($ch, CURLOPT_PROXY, $proxy); 
-	curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
-	curl_setopt($ch, CURLOPT_POST, true);
-	curl_setopt($ch, CURLOPT_POSTFIELDS, $postdata); 
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-	curl_setopt($ch, CURLOPT_HEADER, false);
-	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-	$html = curl_exec($ch);
-	curl_close($ch);
-
-	sleep(2);
-
-	$data = array("id" => "12","jsonrpc" => "2.0","method"=>"Logout","params" => array ());
-	$postdata = json_encode($data);
-
-
-	$ch = curl_init('http://192.168.88.221/jrd/webapi');
+	$ch = curl_init('http://'.$ip.'/jrd/webapi');
 	curl_setopt($ch, CURLOPT_TIMEOUT, 4000);
 	curl_setopt($ch, CURLOPT_PROXY, $proxy); 
 	curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
@@ -161,7 +168,7 @@
 	echo "OK";
 
 
-	function encrypter($str) 
+	function encrypter($str) //функция шифрования для данных
 	{
 		if ($str == "" || $str === undefined) 
 		{
